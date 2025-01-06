bce7c65956c5f6aeee602242f9a547673d50d98e
Wilbert Feldman <will.feldman8@gmail.com>
2025-01-06 17:57:18 -0500

    fix openapi doc gen

--- scripts/generate-openapi.ts
+++ scripts/generate-openapi.ts
@@ -1,5 +1,20 @@
+import 'dotenv/config';
 import { writeFileSync } from 'fs';
 import { join } from 'path';
+import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
+import { z } from "zod";
+
+// Extend Zod with OpenAPI functionality
+extendZodWithOpenApi(z);
+
+// Import all routes to process their OpenAPI registrations
+import '../src/routes/auth';
+import '../src/routes/users';
+import '../src/routes/workspaces';
+import '../src/routes/channels';
+import '../src/routes/messages';
+import '../src/routes/dm';
+
 import { generateOpenApiDocument } from '../src/utils/openapi';
 
 // Generate the OpenAPI document
